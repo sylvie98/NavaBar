cdd301f9c6906b1081b8a67e8a7062a1ada523e5
sylvie98 <sylviemasengesho5@gmail.com>
2024-03-15 10:47:34 +0200

    navigation bar

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const bar = document.querySelector(".fa-bars");
 const menu = document.querySelector(".menu");
 
 bar.addEventListener("click", () => {
-  menu.classList.toggle("show-menu");
+ menu.classList.toggle("show-menu");
 });
